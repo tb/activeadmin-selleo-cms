a9e4947a593d131487fb0002cfd19f24a6384519
aossowski <aossowski@gmail.com>
2012-12-06 22:25:33 +0100

    validations

--- app/models/activeadmin_selleo_cms/page.rb
+++ app/models/activeadmin_selleo_cms/page.rb
@@ -8,6 +8,7 @@ module ActiveadminSelleoCms
 
     validates_presence_of :title, :slug
     validates_uniqueness_of :slug
+    validates_format_of :slug, with: /^[a-z0-9\-_]+$/i
 
     def initialize_missing_translations
       Locale.available_locale_codes.each do |locale_code|
@@ -18,8 +19,9 @@ module ActiveadminSelleoCms
     class Translation
       attr_protected :id
 
-      validates_presence_of :title, :slug, :unless => proc{|t| t.locale == I18n.locale }
-      validates_uniqueness_of :slug, :unless => proc{|t| t.locale == I18n.locale }
+      validates_presence_of :title, :slug, unless: proc{|t| t.locale == I18n.locale }
+      validates_uniqueness_of :slug, unless: proc{|t| t.locale == I18n.locale }
+      validates_format_of :slug, with: /^[a-z0-9\-_]+$/i, unless: proc{|t| t.locale == I18n.locale }
     end
   end
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.setup do |config|
   config.register_javascript 'ckeditor/init.js'
+  config.register_javascript 'activeadmin-selleo-cms/custom.js'
   config.register_stylesheet 'activeadmin-selleo-cms/jquery-ui.css'
 end
